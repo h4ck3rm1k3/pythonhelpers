596153313074c1bb790f3ac2447d8d61d296a17f
Amosse Edouard <eamosse@gmail.com>
2017-01-24 21:36:59 +0100

    First commit

--- helper/TextHelper.py
+++ helper/TextHelper.py
@@ -3,7 +3,10 @@ from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 import string
 import nltk
-
+from autocorrect import spell
+import re
+from helper import MongoHelper as db
+from helper.nerd import  NERD
 tknzr = TweetTokenizer()
 
 stop = stopwords.words('english') + list(string.punctuation)
@@ -24,3 +27,48 @@ class MySentences(object):
             for line in open(fname):
                 yield tokenize(line)
 
+
+def correct(text):
+    text = re.sub(r'(.)\1+', r'\1', text)
+    res = []
+    for t in text.split(" "):
+        res.append(spell(t))
+    return ' '.join(res)
+
+def nerdIt(params,tt):
+    timeout = 10
+    n = NERD('nerd.eurecom.fr', "cci7nqeutkegjsjlb4rrobd9s88vdejl")
+    data = n.extract(tt, 'textrazor', timeout)
+
+    for t in params:
+        t['annotations'] = [d for d in data if d['startChar'] >= t['start'] and d['endChar'] <= t['end']]
+        del t['start']
+        del t['end']
+
+    db.insert("annotation_python", params)
+
+def parseTweets():
+    db.connect("tweets_dataset")
+    limit, skip, index = 10, 0, 0
+    separator = "==="
+    params = []
+
+    while True:
+        tt = ""
+        res = list(db.find("annotated", limit=limit, skip=skip, query={'ontology':'dbpedia', 'type':'normal'}))
+        if len(res) > 0:
+            for r in res:
+                text = str(r['text']).strip()
+                params.append(
+                    {"start": index, "end": len(text) + index + len(separator), 'text': text, 'annotations': [], 'id' : r['tweet_id']})
+                index += len(text) + len(separator)
+                tt += text + separator
+        else:
+            break
+        skip+=limit
+        nerdIt(params,tt)
+
+
+
+if __name__ == '__main__':
+    parseTweets()
